e6d8e954de384a702b6448acf38bdf19beedd2df
BrownKnight <amanrk@hotmail.co.uk>
2020-09-09 23:23:15 +0100

    Implement basic folder structer for ENKEL api

--- ENKEL/STRUDAL/UserLoginDBO.ts
+++ ENKEL/STRUDAL/UserLoginDBO.ts
@@ -1,19 +1,21 @@
-import "reflext-metadata";
+import "reflect-metadata";
 import { createConnection } from "typeorm";
 
 import { UserLogin } from "./entity/UserLogin";
 
 
-export class DBO {
-    private createConnection({
-        type: "postgres",
-        host: process.env.STRUDAL_DATABASE_HOST,
-        username: process.env.STRUDAL_DATABASE_USERNAME,
-        password: process.env.STRUDAL_DATABASE_PASSWORD,
-        database: "STRUDELDB",
-        entities: [
-            UserLogin
-        ],
-        synchronize: true
-    })
+export class UserLoginDBO {
+    private conn() {
+        return createConnection({
+            type: "postgres",
+            host: process.env.STRUDAL_DATABASE_HOST,
+            username: process.env.STRUDAL_DATABASE_USERNAME,
+            password: process.env.STRUDAL_DATABASE_PASSWORD,
+            database: "STRUDELDB",
+            entities: [
+                UserLogin
+            ],
+            synchronize: true
+        })
+    }
 }
--- ENKEL/STRUDAL/entity/UserLogin.ts
+++ ENKEL/STRUDAL/entity/UserLogin.ts
@@ -1,4 +1,4 @@
-import {Entity, Column, PrimaryGeneratedColumn} from "typeorm";
+import {Entity, Column} from "typeorm";
 
 @Entity()
 export class UserLogin {
