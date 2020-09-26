0d6f92d9e9e7ad77fc96427bbbbbcfcac05849ea
BrownKnight <amanrk@hotmail.co.uk>
2020-09-26 19:40:41 +0100

    Add unique constrant to teamName

--- ENKEL/STRUDAL/entity/Team.ts
+++ ENKEL/STRUDAL/entity/Team.ts
@@ -1,11 +1,12 @@
 import pkg from "typeorm";
-const { PrimaryGeneratedColumn, Entity, Column } = pkg;
+const { PrimaryGeneratedColumn, Entity, Column, Index } = pkg;
 
 @Entity()
 export class Team {
   @PrimaryGeneratedColumn()
-  id!: number;
+  id!: number | undefined;
 
   @Column("character varying")
+  @Index({ unique: true })
   teamName!: string;
 }
