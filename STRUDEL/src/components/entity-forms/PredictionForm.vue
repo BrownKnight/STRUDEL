054144bbfaed3abc86a128e5ab20336635a5f942
BrownKnight <amanrk@hotmail.co.uk>
2020-10-15 21:05:00 +0100

    fixes #11 by allowing for fixtures and user to be selected by drop down for fixtures

--- STRUDEL/src/components/entity-forms/PredictionForm.vue
+++ STRUDEL/src/components/entity-forms/PredictionForm.vue
@@ -9,12 +9,17 @@
 
         <label class="sr-only" id="input-label-fixture" for="input-fixture">Fixture</label>
         <b-input-group class="my-3" prepend="Fixture">
-          <b-form-input id="input-fixture" v-model.number="entityModel.fixture.id" required></b-form-input>
+          <b-form-select
+            id="input-fixture"
+            v-model="entityModel.fixture.id"
+            :options="fixtureOptions"
+            required
+          ></b-form-select>
         </b-input-group>
 
         <label class="sr-only" id="input-label-user" for="input-user">User</label>
         <b-input-group class="my-3" prepend="User">
-          <b-form-input id="input-user" v-model.number="entityModel.user.id" required></b-form-input>
+          <b-form-select id="input-user" v-model="entityModel.user.id" :options="userOptions" required></b-form-select>
         </b-input-group>
 
         <label class="sr-only" id="input-label-prediction" for="input-prediction">Prediction</label>
@@ -38,6 +43,8 @@ import { Component } from "vue-property-decorator";
 import { EntityForm } from "@/components/entity-forms/EntityForm.ts";
 import { Prediction } from "@/ENKEL/entity/Prediction";
 import { NewEntity } from "../BaseComponent";
+import { UserLogin } from "@/ENKEL/entity/UserLogin";
+import { Fixture } from "@/ENKEL/entity/Fixture";
 
 @Component({
   components: {}
@@ -50,11 +57,51 @@ export default class PredictionForm extends EntityForm {
     { value: "A", text: "Away Win" }
   ];
 
+  userOptions = [];
+  fixtureOptions = [];
+
+  created() {
+    this.populateUserList();
+    this.populateFixtureList();
+  }
+
   createNewEntityModel(value: Prediction & NewEntity) {
     value.new = true;
     value.fixture = {};
     value.user = {};
     return value;
+  }
+
+  populateUserList() {
+    this.callENKEL("/iapi/users")
+      .then(res => res.json())
+      .then(json => {
+        json = (json as Array<UserLogin>).sort((a, b) => a.fullName.localeCompare(b.fullName));
+
+        this.userOptions = json.map((entity: UserLogin) => {
+          return {
+            value: entity.id,
+            text: `${entity.fullName} (${entity.id})`
+          };
+        });
+      });
+  }
+
+  populateFixtureList() {
+    this.callENKEL("/iapi/fixtures")
+      .then(res => res.json())
+      .then(json => {
+        json = (json as Array<Fixture>).sort((a, b) => {
+          return new Date(b.date).getTime() - new Date(a.date).getTime();
+        });
+
+        this.fixtureOptions = json.map((entity: Fixture) => {
+          return {
+            value: entity.id,
+            text: `${entity.date}: ${entity.homeTeam.teamName} v ${entity.awayTeam.teamName}`
+          };
+        });
+      });
   }
 }
 </script>
