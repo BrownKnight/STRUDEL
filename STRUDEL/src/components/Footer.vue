3b6640bfb29e5069c57730459752bba7d4500006
BrownKnight <amanrk@hotmail.co.uk>
2020-10-20 21:15:57 +0100

    Add version number to footer

--- STRUDEL/src/components/Footer.vue
+++ STRUDEL/src/components/Footer.vue
@@ -7,19 +7,18 @@
       </p>
     </b-modal>
 
-    <b-row class="px-0">
-      <b-col cols="6" md="4" offset-md="4" class="text-left text-md-center">
-        <b-button size="sm" variant="outline-primary" v-b-modal.issue-modal>
-          <b-icon icon="bug" class="align-top mr-1"></b-icon>
-          Found an issue?
-        </b-button>
-      </b-col>
-      <b-col cols="6" md="3" class="text-right" v-if="isAdmin()">
-        <b-button size="sm" variant="outline-primary" href="/api/docs">
-          <b-icon icon="newspaper" class="align-top mr-1"></b-icon>
-          API Docs
-        </b-button>
-      </b-col>
+    <b-row class="px-0 justify-content-between align-items-center mx-1 mx-md-5">
+      <b-button size="sm" variant="outline-primary" href="/api/docs">
+        <b-icon icon="newspaper" class="align-top mr-1"></b-icon>
+        API Docs
+      </b-button>
+      <b-button size="sm" variant="outline-primary" v-b-modal.issue-modal>
+        <b-icon icon="bug" class="align-top mr-1"></b-icon>
+        Found an issue?
+      </b-button>
+      <b-link :href="`https://github.com/BrownKnight/STRUDEL/releases/tag/${versionNumber}`">
+        <small>{{ versionNumber }}</small>
+      </b-link>
     </b-row>
   </footer>
 </template>
@@ -36,6 +35,10 @@ export default class Header extends BaseComponent {
 
   get isLoggedIn() {
     return this.$store.state?.AuthModule?.token !== null;
+  }
+
+  get versionNumber() {
+    return process.env.VUE_APP_GITHUB_REF;
   }
 
   invalidateToken() {
