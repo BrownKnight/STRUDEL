f05aaaab54413aa3fd81efdb71774f98a96eed30
BrownKnight <amanrk@hotmail.co.uk>
2020-10-29 21:48:24 +0000

    Implement form for sending task creation requests to APPLE

---
+++ STRUDEL/src/components/APPLEManagement.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="apple-management">
+    <b-form @submit.prevent="submit()">
+      <b-row>
+        <b-col cols="6" md="6" class="text-left">
+          <b-form-group label="Job Name:" label-size="sm">
+            <b-form-input v-model="appleParameters.jobName" required />
+          </b-form-group>
+        </b-col>
+
+        <b-col cols="6" md="6" class="text-left">
+          <b-form-group label="Week Number:" label-size="sm">
+            <b-form-input type="number" v-model="appleParameters.weekNumber" required />
+          </b-form-group>
+        </b-col>
+
+        <b-col cols="12" class="text-left">
+          <b-form-group label="Fixtures Between:" label-size="sm">
+            <b-input-group>
+              <DatePicker id="start-date" v-model="appleParameters.startDate" required />
+              <b-input-group-text class="no-border-radius">and</b-input-group-text>
+              <DatePicker id="end-date" v-model="appleParameters.endDate" required />
+            </b-input-group>
+          </b-form-group>
+        </b-col>
+
+        <b-col cols="12" class="text-left">
+          <b-form-group label="Data for Predictions:" label-size="sm">
+            <b-form-input v-model="appleParameters.predictionData" required />
+          </b-form-group>
+        </b-col>
+
+        <b-col cols="6">
+          <b-form-checkbox v-model="appleParameters.backTest" switch>Back Test</b-form-checkbox>
+        </b-col>
+        <b-col cols="6">
+          <b-form-checkbox v-model="appleParameters.cleanup" switch>Cleanup</b-form-checkbox>
+        </b-col>
+
+        <b-col class="mt-2">
+          <b-button variant="success" type="submit">Start APPLE</b-button>
+        </b-col>
+      </b-row>
+    </b-form>
+  </div>
+</template>
+
+<script lang="ts">
+import "reflect-metadata";
+import { Component } from "vue-property-decorator";
+import { BaseComponent } from "@/components/BaseComponent.ts";
+import DatePicker from "@/components/helpers/DatePicker.vue";
+import moment from "moment";
+
+@Component({
+  components: { DatePicker }
+})
+export default class APPLEManagement extends BaseComponent {
+  appleParameters = {
+    startDate: moment()
+      .startOf("week")
+      .format("YYYY-MM-DD"),
+
+    endDate: moment()
+      .add(1, "week")
+      .format("YYYY-MM-DD"),
+
+    predictionData: "",
+    weekNumber: null,
+    jobName: "",
+    backTest: false,
+    cleanup: false
+  };
+
+
+  submit() {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.no-border-radius {
+  border-radius: 0;
+  margin-left: -1px;
+  margin-right: -2px;
+}
+</style>
--- STRUDEL/src/pages/Management.vue
+++ STRUDEL/src/pages/Management.vue
@@ -6,47 +6,72 @@
     <b-row>
       <b-col cols="12" md="6" offset-md="3">
         <b-list-group>
-          <b-list-group-item button v-b-toggle.generate-predictions-for-user variant="light">
-            <span>Generate Predictions for a User</span>
+          <b-list-group-item
+            button
+            @click="showGeneratePredictions = !showGeneratePredictions"
+            :active="showGeneratePredictions"
+            class="d-flex px-2"
+          >
+            <b-col cols="10" offset="1">
+              <span>Generate Predictions for a User</span>
+            </b-col>
+            <b-col cols="1" class="">
+              <b-icon :icon="showGeneratePredictions ? 'chevron-up' : 'chevron-down'" />
+            </b-col>
           </b-list-group-item>
-          <b-collapse id="generate-predictions-for-user">
-            <b-list-group-item>
-              <b-row>
-                <b-col cols="12" md="6" class="mt-1">
-                  <b-input-group prepend="User">
-                    <b-form-select v-model="selectedUserId" :options="userOptions"></b-form-select>
-                  </b-input-group>
-                </b-col>
-                <b-col cols="12" md="6" class="mt-1">
-                  <b-input-group prepend="Date">
-                    <b-form-input
-                      v-if="hasNativeDatePicker()"
-                      v-model="date"
-                      type="date"
-                      id="filter-end-date"
-                    ></b-form-input>
-                    <b-form-datepicker
-                      v-else
-                      v-model="date"
-                      id="filter-end-date"
-                      :date-format-options="{ year: 'numeric', month: 'numeric', day: 'numeric' }"
-                    ></b-form-datepicker>
-                  </b-input-group>
-                </b-col>
-                <b-col cols="12" class="mt-2">
-                  <b-button variant="success" size="sm" @click="generatePredictionsForUser()">
-                    Generate Empty Predictions
-                  </b-button>
-                </b-col>
-              </b-row>
-            </b-list-group-item>
-          </b-collapse>
+          <b-list-group-item v-if="showGeneratePredictions" class="px-3 pt-0">
+            <b-row>
+              <b-col cols="12" md="6" class="mt-3">
+                <b-input-group prepend="User">
+                  <b-form-select v-model="selectedUserId" :options="userOptions"></b-form-select>
+                </b-input-group>
+              </b-col>
+              <b-col cols="12" md="6" class="mt-3">
+                <b-input-group prepend="Date">
+                  <b-form-input
+                    v-if="hasNativeDatePicker()"
+                    v-model="date"
+                    type="date"
+                    id="filter-end-date"
+                  ></b-form-input>
+                  <b-form-datepicker
+                    v-else
+                    v-model="date"
+                    id="filter-end-date"
+                    :date-format-options="{ year: 'numeric', month: 'numeric', day: 'numeric' }"
+                  ></b-form-datepicker>
+                </b-input-group>
+              </b-col>
+              <b-col cols="12" class="mt-2">
+                <b-button variant="success" size="sm" @click="generatePredictionsForUser()">
+                  Generate Empty Predictions
+                </b-button>
+              </b-col>
+            </b-row>
+          </b-list-group-item>
 
-          <b-list-group-item to="maintain-analytics" variant="light">
+          <b-list-group-item to="maintain-analytics">
             <span>Manage Analytics Items</span>
           </b-list-group-item>
-          <b-list-group-item to="maintain-fixtures" variant="light">
+          <b-list-group-item to="maintain-fixtures">
             <span>Manage Fixtures</span>
+          </b-list-group-item>
+
+          <b-list-group-item
+            button
+            @click="showManageApple = !showManageApple"
+            :active="showManageApple"
+            class="d-flex px-2"
+          >
+            <b-col cols="10" offset="1">
+              <span>APPLE</span>
+            </b-col>
+            <b-col cols="1" class="">
+              <b-icon :icon="showManageApple ? 'chevron-up' : 'chevron-down'" />
+            </b-col>
+          </b-list-group-item>
+          <b-list-group-item v-if="showManageApple">
+            <APPLEManagement />
           </b-list-group-item>
         </b-list-group>
       </b-col>
@@ -56,18 +81,22 @@
 
 <script lang="ts">
 import { BaseComponent } from "@/components/BaseComponent";
+import APPLEManagement from "@/components/APPLEManagement.vue";
 import { UserLogin } from "@/ENKEL/entity/UserLogin";
 import moment from "moment";
 import "reflect-metadata";
 import { Component } from "vue-property-decorator";
 
 @Component({
-  components: {}
+  components: { APPLEManagement }
 })
 export default class Management extends BaseComponent {
   selectedUserId = "";
   userOptions: Array<{ value: number | null; text: string }> = [];
   date = moment().format("YYYY-MM-DD");
+
+  showGeneratePredictions = false;
+  showManageApple = false;
 
   created() {
     this.populateUserList();
