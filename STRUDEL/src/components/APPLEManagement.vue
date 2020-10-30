ecc1b69d4eb376457f5d22407897cd4786aaf065
BrownKnight <amanrk@hotmail.co.uk>
2020-10-30 21:47:52 +0000

    Add more APPLE states

--- STRUDEL/src/components/APPLEManagement.vue
+++ STRUDEL/src/components/APPLEManagement.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="apple-management">
-    <b-collapse v-model="showForm">
+    <b-collapse :visible="isStateNotSubmitted">
       <b-form @submit.prevent="submit()">
         <b-row>
           <b-col cols="6" md="6" class="text-left">
@@ -45,12 +45,29 @@
       </b-form>
     </b-collapse>
 
-    <b-collapse v-model="showProgress">
+    <b-collapse :visible="isStateTypeInProgress">
       <b-col cols="12" class="mb-3 mt-2">
         <b-spinner />
       </b-col>
-      <b-badge variant="warning">Submitted</b-badge>
+      <b-badge variant="warning" v-if="isStateSubmitted">SUBMITTED</b-badge>
+      <b-badge variant="warning" v-if="isStateWorking">WORKING</b-badge>
     </b-collapse>
+
+    <b-collapse :visible="isStateComplete">
+      <b-badge variant="success">COMPLETE</b-badge>
+    </b-collapse>
+
+    <b-collapse :visible="isStateFailed">
+      <b-badge variant="danger">FAILED</b-badge>
+    </b-collapse>
+
+    <b-button-group>
+      <b-button @click="APPLEState = APPLEStates.NotSubmitted">NotSubmitted</b-button>
+      <b-button @click="APPLEState = APPLEStates.Submitted">Submitted</b-button>
+      <b-button @click="APPLEState = APPLEStates.Working">Working</b-button>
+      <b-button @click="APPLEState = APPLEStates.Complete">Complete</b-button>
+      <b-button @click="APPLEState = APPLEStates.Failed">Failed</b-button>
+    </b-button-group>
   </div>
 </template>
 
@@ -60,6 +77,14 @@ import { Component } from "vue-property-decorator";
 import { BaseComponent } from "@/components/BaseComponent.ts";
 import DatePicker from "@/components/helpers/DatePicker.vue";
 import moment from "moment";
+
+enum States {
+  NotSubmitted,
+  Submitted,
+  Working,
+  Complete,
+  Failed
+}
 
 @Component({
   components: { DatePicker }
@@ -80,14 +105,31 @@ export default class APPLEManagement extends BaseComponent {
     backTest: false,
     cleanup: false
   };
+  APPLEStates = States;
+  APPLEState: States = States.NotSubmitted;
 
-  showForm = true;
-  get showProgress() {
-    return !this.showForm;
+  get isStateNotSubmitted() {
+    return this.APPLEState == States.NotSubmitted;
+  }
+  get isStateSubmitted() {
+    return this.APPLEState == States.Submitted;
+  }
+  get isStateWorking() {
+    return this.APPLEState == States.Working;
+  }
+  get isStateComplete() {
+    return this.APPLEState == States.Complete;
+  }
+  get isStateFailed() {
+    return this.APPLEState == States.Failed;
+  }
+
+  get isStateTypeInProgress() {
+    return this.isStateSubmitted || this.isStateWorking;
   }
 
   submit() {
-    this.showForm = false;
+    this.APPLEState = States.Submitted;
   }
 }
 </script>
