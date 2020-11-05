3eeae326ea0ee1baf61373019e36c3e411026596
BrownKnight <amanrk@hotmail.co.uk>
2020-11-05 21:23:53 +0000

    Dynamically size analytics items

--- STRUDEL/src/components/AnalyticsItemsView.vue
+++ STRUDEL/src/components/AnalyticsItemsView.vue
@@ -1,57 +1,91 @@
 <template>
-  <div id="analytics-items-list">
-    <template v-for="entity in entityList">
-      <b-card class="shadow mt-3" :key="entity.id">
-        <h3>{{ entity.heading }}</h3>
-        <b-container>
-          <div class="iframe-container">
-            <iframe :srcdoc="entity.html" frameborder="0"></iframe>
+  <b-container id="analytics-items-list" fluid>
+    <b-row>
+      <template v-for="entity in entityList">
+        <b-col cols="12" :lg="entity._expanded ? '12' : '6'" :key="entity.id">
+          <div class="d-flex justify-content-between align-items-center">
+            <h4 class="text-left">{{ entity.heading }}</h4>
+            <b-link @click="handleFrameExpand(entity)">
+              <b-icon :icon="entity._expanded ? 'dash' : 'plus'" font-scale="1.2" />
+            </b-link>
           </div>
-        </b-container>
-        <ul>
-          <li v-for="tagLine in entity.tagLineList.split(',')" :key="tagLine">{{ tagLine }}</li>
-        </ul>
-      </b-card>
-    </template>
-  </div>
+          <div class="border rounded-lg">
+            <iframe
+              :id="`analytics-iframe-${entity.id}`"
+              :srcdoc="entity.html"
+              frameborder="0"
+              @load="handleIFrameLoad"
+              width="100%"
+            ></iframe>
+          </div>
+          <ul>
+            <li v-for="tagLine in entity.tagLineList.split(',')" :key="tagLine">{{ tagLine }}</li>
+          </ul>
+          <hr />
+        </b-col>
+      </template>
+    </b-row>
+  </b-container>
 </template>
 
 <script lang="ts">
 import "reflect-metadata";
 import { Component } from "vue-property-decorator";
 import { BaseComponent } from "./BaseComponent";
-import { AnalyticsItems } from "@/ENKEL/entity/AnalyticsItems";
+import type { AnalyticsItems } from "@/ENKEL/entity/AnalyticsItems";
+
+type AnalyticsItem = AnalyticsItems & {
+  _expanded: boolean;
+}
 
 @Component({})
 export default class AnalyticsItemsView extends BaseComponent {
-  entityList: Array<AnalyticsItems> = [];
+  entityList: Array<AnalyticsItem> = [];
 
   created() {
     this.callENKEL("/iapi/analytics")
       .then(res => res.json())
       .then(json => {
         if (json !== null) {
+          json.forEach((element: AnalyticsItem) => {
+            element._expanded = false;
+          });
           this.entityList = json;
         }
       });
+  }
+
+  handleFrameExpand(entity: AnalyticsItem) {
+    entity._expanded = !entity._expanded;
+    const element = document.getElementById(`analytics-iframe-${entity.id}`) as HTMLIFrameElement;
+    this.resizeIframe(element);
+  }
+
+  handleIFrameLoad(event: Event) {
+    this.resizeIframe(event.target as HTMLIFrameElement);
+  }
+
+  resizeIframe(iframe: HTMLIFrameElement) {
+    console.log("resize");
+    iframe.style.height = iframe?.contentWindow?.document.documentElement.scrollHeight + "px";
   }
 }
 </script>
 
 <style scoped lang="scss">
-.iframe-container {
-  overflow: hidden;
-  /* 16:9 aspect ratio */
-  padding-top: 56.25%;
-  position: relative;
-}
+// .iframe-container {
+//   overflow: hidden;
+//   /* 16:9 aspect ratio */
+//   // padding-top: 56.25%;
+//   position: relative;
+// }
 
-.iframe-container iframe {
-  border: 0;
-  height: 100%;
-  left: 0;
-  position: absolute;
-  top: 0;
-  width: 100%;
-}
+// .iframe-container iframe {
+//   border: 0;
+//   // height: 100%;
+//   left: 0;
+//   position: absolute;
+//   top: 0;
+//   width: 100%;
+// }
 </style>
