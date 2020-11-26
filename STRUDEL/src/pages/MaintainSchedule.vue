717577316e5b70d5a53423c84570e1ad9e0d8fc4
BrownKnight <amanrk@hotmail.co.uk>
2020-11-26 17:11:59 +0000

    cleanup schedule management page

--- STRUDEL/src/pages/MaintainSchedule.vue
+++ STRUDEL/src/pages/MaintainSchedule.vue
@@ -10,10 +10,6 @@
       :fields="fields"
       :populateNewEntity="populateNewEntity"
     ></EntityManagement>
-
-    <b-row v-if="isAdmin()">
-      <h3 class="my-5 mx-3">Management</h3>
-    </b-row>
   </div>
 </template>
 
@@ -23,7 +19,6 @@ import { Component } from "vue-property-decorator";
 import EntityManagement from "@/components/EntityManagement.vue";
 import ScheduleForm from "@/components/entity-forms/ScheduleForm.vue";
 import { BaseComponent, BootstrapTableField } from "@/components/BaseComponent.ts";
-import { Schedule } from "@/ENKEL/entity/Schedule";
 
 @Component({
   components: { EntityManagement, ScheduleForm }
@@ -35,12 +30,9 @@ export default class MaintainSchedule extends BaseComponent {
     { key: "dateModified", sortable: true, label: "Date Modified", formatter: this.prettyFormatDateTime },
     { key: "name", sortable: true, label: "Schedule Name" },
     { key: "jobName", sortable: true, label: "Job Name" },
-    { key: "cron", sortable: true, label: "CRON String" }
+    { key: "cron", sortable: true, label: "CRON String" },
+    { key: "Action", label: "Action" }
   ];
-
-  populateNewEntity(entity: Schedule) {
-    console.log("populating");
-  }
 }
 </script>
 
